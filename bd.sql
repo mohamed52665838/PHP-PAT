df3078bea8f7c6b239487c3735dfd129ccf2a2d2
mohamed52665838 <mohamed.dhaouedi@esprit.tn>
2025-08-06 17:18:49 +0100

    Add initial SQL script for patisserie database and tables

---
+++ bd.sql
@@ -0,0 +1,49 @@
+
+CREATE DATABASE IF NOT EXISTS patisserie_db;
+
+-- Utiliser la base de données
+USE patisserie_db;
+
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255),
+    password TEXT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    role ENUM('admin', 'client','preparateur') NOT NULL DEFAULT 'client',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE produits(
+    id int primary key AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    description TEXT,
+    prix DECIMAL(10, 2) NOT NULL,
+    image_url VARCHAR(255),
+    stock INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE commandes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    status ENUM('en attente', 'en cours', 'livrée', 'annulée') NOT NULL DEFAULT 'en attente',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);  
+
+-- CHN3REF BIHA EL COMMANDE IL 5ARJA CHFIHA PRODUITS O UTILISATEUR O N3REF BIHA PROX TOTALE DU COMMANDES
+CREATE TABLE commande_produit(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    commande_id INT NOT NULL,
+    produit_id INT NOT NULL,
+    prixFinaleDeProduit DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (commande_id) REFERENCES commandes(id) ON DELETE CASCADE,
+    FOREIGN KEY (produit_id) REFERENCES produits(id) ON DELETE CASCADE
+);
+
